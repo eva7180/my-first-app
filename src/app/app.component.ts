28a14a40126b3f232ca7921184672148a861f699
Eva Stolz <evasmail@gmail.com>
2019-07-17 18:32:18 +0200

    Set up boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{ title }}</h1>
+  `
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit {
+  title: string;
+  constructor() { }
+
+  ngOnInit() {
+    // We call a service that gets us the data
+    this.title = 'Hello World';
+  }
 }
